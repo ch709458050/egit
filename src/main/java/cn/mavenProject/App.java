39d6e807d5e4b9053d134e20eac0aa14e482f110
chenhuang <709458050@qq.com>
2019-03-20 20:01:05 +0800

    1

--- src/main/java/cn/mavenProject/App.java
+++ src/main/java/cn/mavenProject/App.java
@@ -16,5 +16,6 @@ public class App
         System.out.println( "Hello World!" );
          System.out.println( "Hello World!chenhuang" );
         System.out.println( "Hello World!chenhuang2" );
+        System.out.println( "Hello World!chenhuang3" );
     }
 }
