5c59243484a0f43d229de5d44e7c43d67d5736ff
chenhuang <709458050@qq.com>
2019-03-20 19:40:07 +0800

    4

---
+++ src/main/java/cn/mavenProject/App.java
@@ -0,0 +1,18 @@
+package cn.mavenProject;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App
+{
+    public static void main( String[] args ) throws ClassNotFoundException
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("初始化驱动成功");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cn/mavenProject/test.java
@@ -0,0 +1,5 @@
+package cn.mavenProject;
+
+public class test {
+
+}
