42fc2bd0735670fd82258e9f6f0c01e4a98c8190
ch709458050 <41884101+ch709458050@users.noreply.github.com>
2019-03-20 19:48:48 +0800

    chenhuang
    asd

--- src/main/java/cn/mavenProject/App.java
+++ src/main/java/cn/mavenProject/App.java
@@ -14,5 +14,6 @@ public class App
         Class.forName("com.mysql.jdbc.Driver");
         System.out.println("初始化驱动成功");
         System.out.println( "Hello World!" );
+         System.out.println( "Hello World!chenhuang" );
     }
 }
